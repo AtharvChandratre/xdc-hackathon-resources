ea30f42e4b5903dcabbf796579dbfeaa3f70c25c
Atharv Chandratre <atharvchandratre@gmail.com>
2024-01-29 14:08:53 -0600

    Initial Commit

---
+++ src/app/data.js
@@ -0,0 +1,46 @@
+export const linkData = [{
+    url: "https://docs.xdc.community/",
+    title: "Docs",
+    description: "Find in-depth information about how to build on XDC"
+    },
+    {
+        url: "https://www.xdc.dev/",
+        title: "Dev Forum",
+        description: "Developers Forum for the XDC Network"
+    },
+    {
+        url: "https://xdc.network/",
+        title: "Block Explorer",
+        description: "Block Explorer for the XDC Mainnet and Apothem Testnet"
+    },
+    {
+        url: "https://faucet.blocksscan.io/",
+        title: "Faucet",
+        description: "Faucet for obtaining test XDC for your creations"
+    },
+    {
+        url: "https://docs.xdc.community/get-details/wallet-integration/metamask",
+        title: "Metamask Instructions",
+        description: "Instructions on how to set up the XDC Network in Metamask"
+    },
+    {
+        url: "https://apidocs.xinfin.network/docs/#xinfin-apis",
+        title: "XDC API Docs",
+        description: "Playground to test XDC RPC APIs"
+    },
+    {
+        url: "https://remix.xinfin.network/",
+        title: "XDC Remix",
+        description: "Remix Web IDE with built-in integrations for the XDC Network"
+    },
+    {
+        url: "https://www.youtube.com/watch?v=7pRO-T1JHlY",
+        title: "XDC Workshop",
+        description: "XDC Workshop on how to get started for the hackathon"
+    },
+    // {
+    //     url: "",
+    //     title: "",
+    //     description: ""
+    // },
+]
